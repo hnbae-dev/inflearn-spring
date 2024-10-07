9fb49e104e2e4a1cae4d5a8d9cc61616b6e84698
hnbae-dev <hnbae.dev@gmail.com>
2024-10-07 17:15:39 +0900

    스프링입문-섹션5-스프링빈과의존관계

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,6 +5,8 @@ import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 public class MemberService {
 
