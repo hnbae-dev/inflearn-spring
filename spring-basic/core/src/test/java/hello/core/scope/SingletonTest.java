a83153129b6f3baea587bcfa7912ed0c77c62bc5
hnbae-dev <hnbae.dev@gmail.com>
2024-10-19 08:04:47 +0900

    스프링기본-섹션9-10

---
+++ spring-basic/core/src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,38 @@
+package hello.core.scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+            PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("singletonBean1 = " + prototypeBean1);
+        System.out.println("singletonBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isSameAs(prototypeBean2);
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+
+}
